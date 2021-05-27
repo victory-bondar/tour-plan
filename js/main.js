87fa3d85c88ffe23b00b1c1e9ddd0b7ef79f2169
victory-bondar <victory71.bondar@gmail.com>
2021-05-27 23:43:07 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+  // Effect
+  effect: "coverflow",
+});
