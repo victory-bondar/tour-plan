45e03b73fdc17407c5c556adfbffcf308cb11e0d
victory-bondar <victory71.bondar@gmail.com>
2021-06-08 18:26:22 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -34,3 +34,9 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: "true",
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible'); 
+})
