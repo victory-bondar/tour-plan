ba117efbd2cc638e36877812b6a2be40bd1ab37d
victory-bondar <victory71.bondar@gmail.com>
2021-06-10 12:39:13 +0300

    create: added modal

--- js/main.js
+++ js/main.js
@@ -1,42 +1,64 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
 
-  // Effect
-  effect: "coverflow",
+    // Effect
+    effect: "coverflow",
 
-  //Keyboard arrows
-  keyboard: {
-    enabled: "true",
-    onlyInViewport: "true",
-  },
+    //Keyboard arrows
+    keyboard: {
+      enabled: "true",
+      onlyInViewport: "true",
+    },
+  });
+
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+
+    //Keyboard arrows
+    keyboard: {
+      enabled: "true",
+      onlyInViewport: "true",
+    },
+  });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function () {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible'); 
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  };
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  };
 });
-
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-
-  //Keyboard arrows
-  keyboard: {
-    enabled: "true",
-    onlyInViewport: "true",
-  },
-});
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible'); 
-})
